1c640987a55c64439eb20ae716876428e2fb76ca
Mariia <mkofein@gmail.com>
2022-05-01 14:53:31 +0300

    Create project

---
+++ src/js/01_main.js
@@ -0,0 +1,107 @@
+const pageSlider = new Swiper(".swiper-container", {
+    direction: "vertical",
+    speed: 1000,
+    parallax: true,
+    autoplay: false,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+          shadow: true,
+          origin: "left center",
+          translate: ["-5%", 0, -200],
+          rotate: [0, 100, 0],
+        },
+        next: {
+          origin: "right center",
+          translate: ["5%", 0, -200],
+          rotate: [0, -100, 0],
+        },
+      },
+    mousewheel: true,
+    simulateTouch: false,
+    init: false,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    on: {
+        init: function () {
+            menuSlider();
+            headerButtonMove();
+        },
+        slideChange: function () {
+            menuSlider();
+            headerButtonMove();
+        },
+    },
+});
+
+//Удаление активыных стилей для ссылок
+let menuLinks = document.querySelectorAll('.main-nav__link');
+function menuSliderRemove() {
+    let menuLinkActive = document.querySelectorAll('.main-nav__link.active');
+    if (menuLinkActive) {
+        menuLinkActive.forEach(element => {
+          element.classList.remove('active');
+        });
+    }
+}
+
+//Активные стили для ссылок
+function menuSlider() {
+    menuSliderRemove();
+    console.log(menuLinks);
+    console.log(pageSlider.realIndex);
+    menuLinks[pageSlider.realIndex].classList.add('active');   
+}
+  
+//Стили по клику
+for (let i = 0; i < menuLinks.length; i+=1) {
+    menuLinks[i].onclick = function(e) {
+        e.preventDefault();
+        menuSliderRemove();
+        menuLinks[i].classList.add('active');
+        pageSlider.slideTo(i, 1000);
+  }
+}
+
+//Кнопка на первом слайде
+function headerButtonMove() {
+    const headerButton = document.querySelector('.header__button');
+    const fragment = document.createDocumentFragment();
+	fragment.appendChild(headerButton);
+    if (pageSlider.realIndex === 0) {
+        document.querySelector('.first-screen__wrapper').appendChild(fragment);
+    } else {
+        document.querySelector('.main-nav').appendChild(fragment);
+    }
+}
+
+pageSlider.init();
+
+//Prices
+
+(function() {
+    const pricesOpenButtons = document.querySelectorAll('.forth-screen__header');
+    const pricesCloseButtons = document.querySelectorAll('.forth-screen__body-close');
+    const pricesOderButtons = document.querySelectorAll('.forth-screen__body-footer .button');
+
+    pricesOpenButtons.forEach(element => {
+        element.onclick = function() {
+            element.nextElementSibling.classList.add('show');
+        }
+    });
+
+    pricesCloseButtons.forEach(element => {
+        element.onclick = function() {
+            element.closest('.forth-screen__body').classList.remove('show');
+        }
+    });
+
+    pricesOderButtons.forEach(element => {
+        element.onclick = function() {
+            element.closest('.forth-screen__body').classList.remove('show');
+            pageSlider.slideTo(5, 1000);
+        }
+    });
+})();
